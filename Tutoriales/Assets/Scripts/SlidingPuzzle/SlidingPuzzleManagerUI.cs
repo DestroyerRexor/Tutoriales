31cd3715941566890ac2fffd8f1a359e37bd91d5
Edgar Rodriguez <121734583+DestroyerRexor@users.noreply.github.com>
2025-09-01 14:22:14 -0600

    Tutorial 3 - Sliding Puzzle

--- Tutoriales/Assets/Scripts/SlidingPuzzle/SlidingPuzzleManagerUI.cs
+++ Tutoriales/Assets/Scripts/SlidingPuzzle/SlidingPuzzleManagerUI.cs
@@ -34,7 +34,7 @@ public class SlidingPuzzleManagerUI : MinigamesBase
         base.Hide();
 
         slidingPuzzleGroup.OnCompleteGame -= OnCompleteGame;
-        TimerUI.Instance.OnTimeOver += OnLoseGame;
+        TimerUI.Instance.OnTimeOver -= OnLoseGame;
     }
 
     private void OnCompleteGame()
